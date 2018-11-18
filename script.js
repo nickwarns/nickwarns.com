efc6d8b193fc8f298f0406d81171c392d8d54a75
nickwarns <45149531+nickwarns@users.noreply.github.com>
2018-11-18 17:18:39 -0500

    removed links
    removed link to email and link to page that opens in new tab. did not delete

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function callDog (dogsname){
+  console.log('Come here, '+dogsname + '!')
+}
+callDog ('Mack');
+
+function addNumbers(c,d){
+  console.log (c);
+  console.log (d);
+  console.log (c+d)
+}
+var c = 100;
+var d = 90;
+addNumbers(c, d);
+
+function squaredSum (a,b) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+function DogFact() {
+  console.log ('Dogs rule')
+}
+
+DogFact();
+DogFact();
+DogFact();
+DogFact();
+
+
+
+
 // console.log('I am fuzzy')
 
 // alert('sup dawg')
@@ -5,7 +45,13 @@
 
 
 var numberOfBoats = 25;
+var numberOfFish = 100
+var numberOfFishPerBoat = numberOfFish / numberOfBoats
+console.log(numberOfFishPerBoat);
 
-console.log(numberOfBoats);
+document.write(numberOfFishPerBoat);
 
-document.write(numberOfBoats);
+var dogsname = "Mack"
+var fullname = dogsname + 'Attack'
+
+console.log(fullname);
